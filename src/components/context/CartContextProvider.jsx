78ea0f63744933f6eadb12b1424946cf089db646
darwinQSot <quispesotodaniel@gmail.com>
2023-05-14 23:14:55 -0500

    Add context file

---
+++ src/components/context/CartContextProvider.jsx
@@ -0,0 +1,72 @@
+import { createContext } from 'react';
+import { useState } from 'react';
+
+export const MiContexto = createContext();
+
+const { Provider } = MiContexto;
+
+export default function CartContextProvider({ children }) {
+	const [cart, setCart] = useState([]);
+
+	const addItemCarrito = (item, cantidad) => {
+		const newItem = {
+			...item,
+			cantidad,
+		};
+
+		if (!estaEnCarrito(newItem.id)) {
+			setCart([...cart, newItem]);
+		} else {
+			const findProduct = cart.find((producto) => producto.id === newItem.id);
+			const productIndex = cart.indexOf(findProduct);
+			const auxArray = [...cart];
+
+			if (newItem.cantidad + findProduct.cantidad <= findProduct.stock) {
+				auxArray[productIndex].cantidad += cantidad;
+				setCart(auxArray);
+			}
+		}
+	};
+
+	//. Usaremos en cart. Retorna un nuevo array sin el producto eliminado.
+	const removerItemCarrito = (id) => {
+		return setCart(cart.filter((producto) => producto.id !== id));
+	};
+
+	//. Usaremos en cart. Vacia el carrito.
+	const limpiarCarrito = () => {
+		setCart([]);
+	};
+
+	//. Usaremos en ItemDetail - Informa si el item esta en el carrito o no. Retorna un boolean.
+	const estaEnCarrito = (id) => {
+		return cart.some((item) => item.id === id);
+	};
+
+	//. Usaremos en cartWidget. Nos va a dar la cantidad total de productos en el carrito (state). Es mejor usar el metodo reduce?
+	const cantidadCarrito = () => {
+		return cart.reduce((acc, producto) => (acc += producto.cantidad), 0);
+	};
+
+	//. Usareoms en el cart. Retorna el precio total del carrito.
+	const valorTotalCarrito = () => {
+		return cart.reduce((previousValue, currentValue) => {
+			return previousValue + currentValue.precio * currentValue.cantidad;
+		}, 0);
+	};
+
+	return (
+		<Provider
+			value={{
+				cantidadCarrito,
+				estaEnCarrito,
+				cart,
+				addItemCarrito,
+				removerItemCarrito,
+				limpiarCarrito,
+				valorTotalCarrito,
+			}}>
+			{children}
+		</Provider>
+	);
+}
